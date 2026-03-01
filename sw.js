6c93e680b00bcc94e0ea060b9b36b8cbb33aff54
hatfeals <ma.fe.sa@hotmail.com>
2026-03-01 17:16:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'guide-v1';
+const CACHE_NAME = 'guide-v2';
 const urls = ['./', './index.html', './words.js', './manifest.json'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urls))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
